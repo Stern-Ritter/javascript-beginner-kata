2f592e533de6980c28ec7acc3209886eaedff88c
Stern-Ritter <fadeew.a@mail.ru>
2021-11-12 12:26:39 +0300

    Setup eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,21 +2,24 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    "jest/globals": true
+    "jest/globals": true,
   },
-  extends: [
-    'airbnb-base',
-    'prettier'
-  ],
+  extends: ["airbnb-base", "prettier"],
   parserOptions: {
     ecmaVersion: 13,
-    sourceType: 'module',
+    sourceType: "module",
   },
   rules: {
     "import/prefer-default-export": "off",
-    "max-len": ['error', {
-      ignoreComments: true
-    }]
+    "max-len": [
+      "error",
+      {
+        ignoreComments: true,
+        code: 100,
+      },
+    ],
+    "no-console": "off",
+    "no-alert": "off",
   },
-  "plugins": ["jest"]
+  plugins: ["jest"],
 };
